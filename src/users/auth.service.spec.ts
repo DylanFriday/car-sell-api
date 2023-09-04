c9f58afc0bb66aaaf0ac8308487effd65db5fd1a
Sitt Hmue Eain <sitthmueeain@gmail.com>
2023-09-04 17:10:45 +0630

    refactoring_intelligent_mocks_tests_for_authService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -5,20 +5,29 @@ import { User } from './users.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
-  let fakeUserService: Partial<UsersService>;
+  let fakeUsersService: Partial<UsersService>;
+
   beforeEach(async () => {
     // Create a fake copy of the users service
-    fakeUserService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+    const users : User[] = [];
+    fakeUsersService = {
+      find: (email : string) => {
+        const filteredUsers = users.filter(user => user.email === email)
+        return Promise.resolve(filteredUsers)
+      },
+      create: (email: string, password: string) =>{
+        const user = {id : Math.floor(Math.random() * 9999),email,password} as User
+        users.push(user)
+        return Promise.resolve(user)
+      }
     };
+
     const module = await Test.createTestingModule({
       providers: [
         AuthService,
         {
           provide: UsersService,
-          useValue: fakeUserService,
+          useValue: fakeUsersService,
         },
       ],
     }).compile();
@@ -29,21 +38,46 @@ describe('AuthService', () => {
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
   });
-  it('create a new user with a salted and hashed password', async () => {
-    const user = await service.signUp('asdf@gmail.com', 'asdf');
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signUp('asdf@asdf.com', 'asdf');
+
     expect(user.password).not.toEqual('asdf');
-    const [salted, hashed] = user.password.split('.');
-    expect(salted).toBeDefined();
-    expect(hashed).toBeDefined();
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
   });
 
-  it('throws an error if user signs up with email that is already in use', async (done) => {
-    fakeUserService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+  it('throws an error if user signs up with email that is in use', async () => {
+    await service.signUp('emailTest@gmail.com', 'asdf');
     try {
-      await service.signUp('asddf@gmail.com', 'asdf');
+      await service.signUp('emailTest@gmail.com', 'asdf');
     } catch (err) {
-      done();
+     return err
     }
+  });
+
+  it('throws if signin is called with an unused email', async () => {
+    try {
+      await service.signIn('emailTe123st@gmail.com', 'passdflkj');
+    } catch (err) {
+      return err
+    }
+  });
+  
+
+  it('throws if an invalid password is provided', async () => {
+    await service.signUp('laskdjf@alskdfj.com', 'passowrd123');
+    try {
+      await service.signIn('laskdjf@alskdfj.com', 'passowrd');
+    } catch (err) {
+    return err
+    }
+  });
+
+  it('returns a user if correct password is provided', async () => {
+    await service.signUp('asdf@asdf.com', 'mypassword')
+    const user = await service.signIn('asdf@asdf.com', 'mypassword');
+    expect(user).toBeDefined();
   });
 });
